aabf38d8b6e159ded59067f4060a7c8450ea5b34
boukhimaoscar@gmail.com <boukhimaoscar@gmail.com>
2021-02-09 23:01:42 +0100

    0.3

--- app/src/main/java/com/example/yonococinoapp/fragmentos/ajustesFragment.java
+++ app/src/main/java/com/example/yonococinoapp/fragmentos/ajustesFragment.java
@@ -25,10 +25,6 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class ajustesFragment extends Fragment {
 
-
-
-
-
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
