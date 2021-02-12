f912c7189fa63aa0ff4b69aaffe81411321c9176
boukhimaoscar@gmail.com <boukhimaoscar@gmail.com>
2021-02-12 14:06:23 +0100

    0.3

--- app/src/main/java/com/example/yonococinoapp/fragmentos/ajustesFragment.java
+++ app/src/main/java/com/example/yonococinoapp/fragmentos/ajustesFragment.java
@@ -79,9 +79,9 @@ public class ajustesFragment extends Fragment {
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseDatabase baseDeDatos = FirebaseDatabase.getInstance();
 
-
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_ajustes, container, false);
+
         EditText textEditCambioNombre = (EditText) v.findViewById(R.id.textEditnuevoNombre);
         TextView prueba = (TextView) v.findViewById(R.id.textViewprueba);
         String nuevoNombre = textEditCambioNombre.toString();
@@ -118,6 +118,8 @@ public class ajustesFragment extends Fragment {
 
 
     }
+
+
 
 
 }
